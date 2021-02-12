b6bf68b314309a66be1ae9da58187b97fd124834
bot-vini-ofc-2 <78944640+bot-vini-ofc-2@users.noreply.github.com>
2021-02-11 23:36:14 -0400

    Add files via upload

---
+++ src/help.js
@@ -0,0 +1,117 @@
+const help = (p, date) => {
+	return `●❯───BOT VINI COMANDOS───❮●
+	
+❖ ── ✦ ──『✙』── ✦ ── ❖
+
+┃➤Criador : Vinicius 
+┃➤Numero : https://wa.me/5565992559365
+┃➤Grupo do bot : https://chat.whatsapp.com/GGHuaQQ5cZO7ibeBQjrEJA
+
+❖ ── ✦ ──『✙』── ✦ ── ❖
+
+*╔═══❖•ೋ° °ೋ•❖═══╗*
+
+◎ ━❖★COMANDO PRINCIPAL★❖━ ◎
+
+*╚═══❖•ೋ° °ೋ•❖═══╝*
+*${date}*
+║ ❯ *${p}criador*
+║ ❯ *${p}info*
+║ ❯ *${p}blocklist*
+║ ❯ *${p}stiker* <caption, reply image>
+║ ❯ *${p}sticker* <caption, reply image>
+║ ❯ *${p}toimg* <caption, reply sticker>
+
+*╔═══❖•ೋ° °ೋ•❖═══╗*
+
+◎ ━❖★COMANDO DE TEXTO ★❖━ ◎
+
+*╚═══❖•ೋ° °ೋ•❖═══╝*
+
+║ ❯ *${p}textcomands*
+║ ❯ *${p}repeat* <text>
+║ ❯ *${p}ocr* <caption, reply sticker>
+║ ❯ *${p}nethunter*
+║ ❯ *${p}signome* <name>
+║ ❯ *${p}trava* <qnt> <num>
+║ ❯ *${p}idioma*
+║ ❯ *${p}frase*
+║ ❯ *${p}destrava*
+║ ❯ *${p}wppim*
+║ ❯ *${p}db*
+
+*╔═══❖•ೋ° °ೋ•❖═══╗*
+
+◎ ━❖★COMANDO DE MEME ★❖━ ◎
+
+*╚═══❖•ೋ° °ೋ•❖═══╝*
+
+║ ❯ *${p}meme*
+║ ❯ *${p}memeindo*
+
+*╔═══❖•ೋ° °ೋ•❖═══╗*
+
+◎ ━❖★COMANDO DE AUDIO ★❖━ ◎
+
+*╚═══❖•ೋ° °ೋ•❖═══╝*
+
+║ ❯ *${p}gtts* <la> <text>
+║ ❯ *${p}playlist*
+
+*╔═══❖•ೋ° °ೋ•❖═══╗*
+
+◎ ━❖★COMANDO DE ANIME★❖━ ◎
+
+*╚═══❖•ೋ° °ೋ•❖═══╝*
+
+║ ❯ *${p}randomanime*
+║ ❯ *${p}randomshota*
+║ ❯ *${p}randomkiss*
+║ ❯ *${p}randomhug*
+║ ❯ *${p}nekoanime*
+║ ❯ *${p}wait* <caption, reply image>
+
+*╔═══❖•ೋ° °ೋ•❖═══╗*
+
+◎ ━❖★COMANDO +18 ★❖━ ◎
+
+*╚═══❖•ೋ° °ೋ•❖═══╝*
+
+║ ❯ *${p}packmega18*
+║ ❯ *${p}randomhentai*
+║ ❯ *${p}nsfwblowjob*
+║ ❯ *${p}nsfwneko*
+║ ❯ *${p}nsfwtrap*
+║ ❯ *${p}nsfw* <mode 1 or 0>
+
+*╔═══❖•ೋ° °ೋ•❖═══╗*
+
+◎ ━❖★COMANDO DE GRUPO★❖━ ◎
+
+*╚═══❖•ೋ° °ೋ•❖═══╝*
+
+║ ❯ *${p}welcome* <mode 1 or 0>
+║ ❯ *${p}linkgroup*
+║ ❯ *${p}add* <number>
+║ ❯ *${p}listadmin* <number>
+║ ❯ *${p}kick* <dial number>
+║ ❯ *${p}leave*
+║ ❯ *${p}promote* <dial number>
+║ ❯ *${p}demote* <dial number>
+║ ❯ *${p}simih* <mode 1 or 0>
+║ ❯ *${p}tagall*
+║ ❯ *${p}tagall2*
+║ ❯ *${p}tagall3*
+
+╲╭━<━━╮╲╲
+╲┃╭╮╭╮┃╲╲.    OBG POR USAR O
+┗┫┏━━┓┣┛╲.          BOT VINI
+╲┃╰━━╯┃ ╲
+╲╰┳━━┳╯╲╲
+╲╲┛╲╲┗╲╲╲
+
+}
+
+exports.help = help
+
+
